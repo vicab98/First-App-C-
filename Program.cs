af1fd98c2c8a141e77602940081f6c48d0f5bb60
Victoria Bodiu <40336840+vicab98@users.noreply.github.com>
2021-06-21 16:29:38 +0300

    First C# Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any kew to continue");
+            Console.ReadKey();
+        }
+    }
+}
